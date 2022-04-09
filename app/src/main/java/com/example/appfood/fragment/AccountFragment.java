64750de9f59cbd727dfc05aee21c816d89d67dcc
BaoTrung37 <baotrung372k@gmail.com>
2022-04-09 23:25:08 +0700

    test

--- app/src/main/java/com/example/appfood/fragment/AccountFragment.java
+++ app/src/main/java/com/example/appfood/fragment/AccountFragment.java
@@ -17,6 +17,7 @@ public class AccountFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_account,container,false);
         return view;
+        //
     }
 
     @Override
