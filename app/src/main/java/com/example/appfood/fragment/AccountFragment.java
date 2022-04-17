67cfb54af35681a72c86f6b3abfd15cfba71f837
BaoTrung37 <baotrung372k@gmail.com>
2022-04-17 23:27:48 +0700

    create screen update information

--- app/src/main/java/com/example/appfood/fragment/AccountFragment.java
+++ app/src/main/java/com/example/appfood/fragment/AccountFragment.java
@@ -11,16 +11,39 @@ import androidx.fragment.app.Fragment;
 
 import com.example.appfood.R;
 
-public class AccountFragment extends Fragment {
+import de.hdodenhof.circleimageview.CircleImageView;
+
+public class AccountFragment extends Fragment implements View.OnClickListener {
+    CircleImageView circleInformation;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_account,container,false);
+        View view = inflater.inflate(R.layout.fragment_account, container, false);
         return view;
     }
 
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        initData(view);
+    }
+
+    private void initData(View view) {
+        // find by id
+        circleInformation = view.findViewById(R.id.circle_information);
+        // set event
+        circleInformation.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.circle_information:
+                UpdateInformationFragment updateInformationFragment = new UpdateInformationFragment();
+                getFragmentManager().beginTransaction().replace(R.id.framelayout, updateInformationFragment)
+                        .addToBackStack("updateInformationFragment").commit();
+                break;
+        }
     }
 }
