312bab01be111233524c7616e9972b6e219daebd
BaoTrung37 <baotrung372k@gmail.com>
2022-04-28 17:51:25 +0700

    add screen address fragment

---
+++ app/src/main/java/com/example/appfood/fragment/AddressFragment.java
@@ -0,0 +1,76 @@
+package com.example.appfood.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+
+import com.example.appfood.R;
+import com.google.android.material.textfield.TextInputEditText;
+
+public class AddressFragment extends Fragment implements View.OnClickListener{
+
+    TextInputEditText textInputEditTextAddress;
+    Button btConfirm;
+    SetAddress setAddress;
+
+//    static String address = "";
+//    public static AddressFragment newInstance() {
+//        Bundle args = new Bundle();
+//        args.putString("address",address);
+//        AddressFragment fragment = new AddressFragment();
+//        fragment.setArguments(args);
+//        return fragment;
+//    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_address,container,false);
+        return view;
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        initData(view);
+        setToolBar(view);
+    }
+
+    private void initData(View view) {
+        textInputEditTextAddress = view.findViewById(R.id.textInputEditText_address);
+        btConfirm = view.findViewById(R.id.bt_confirm);
+        btConfirm.setOnClickListener(this);
+    }
+
+    private void setToolBar(View view) {
+        Toolbar toolbar = view.findViewById(R.id.toolBar);
+
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                getParentFragmentManager().popBackStack();
+            }
+        });
+    }
+    public void setSetAddress(SetAddress setAddress){
+        this.setAddress = setAddress;
+    }
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.bt_confirm:
+//                address = textInputEditTextAddress.getText().toString();
+                getParentFragmentManager().popBackStack();
+                setAddress.setAddress(textInputEditTextAddress.getText().toString());
+                break;
+        }
+    }
+
+}
--- app/src/main/java/com/example/appfood/fragment/PaymentFragment.java
+++ app/src/main/java/com/example/appfood/fragment/PaymentFragment.java
@@ -3,8 +3,10 @@ package com.example.appfood.fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.SurfaceControl;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -12,8 +14,27 @@ import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 
 import com.example.appfood.R;
+interface SetAddress{
+    void setAddress(String address);
+}
 
-public class PaymentFragment extends Fragment implements View.OnClickListener {
+public class PaymentFragment extends Fragment implements View.OnClickListener,SetAddress {
+    
+    TextView tvAddAddress,tvAddress;
+
+    public static PaymentFragment newInstance() {
+
+        Bundle args = new Bundle();
+
+        PaymentFragment fragment = new PaymentFragment();
+        fragment.setArguments(args);
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
 
     @Nullable
     @Override
@@ -26,9 +47,10 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         initData(view);
+        setToolBar(view);
     }
 
-    private void initData(View view) {
+    private void setToolBar(View view) {
         Toolbar toolbar = view.findViewById(R.id.toolBar);
 
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
@@ -39,8 +61,29 @@ public class PaymentFragment extends Fragment implements View.OnClickListener {
         });
     }
 
+    private void initData(View view) {
+        tvAddAddress = view.findViewById(R.id.tv_add_address);
+        tvAddress = view.findViewById(R.id.tv_address);
+
+        tvAddAddress.setOnClickListener(this);
+    }
+
     @Override
     public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.tv_add_address:
+                AddressFragment addressFragment = new AddressFragment();
+                addressFragment.setSetAddress(this);
+                getParentFragmentManager().beginTransaction()
+                        .add(R.id.framelayout,addressFragment)
+                        .addToBackStack("addressFragment")
+                        .commit();
+                break;
+        }
+    }
 
+    @Override
+    public void setAddress(String address) {
+        tvAddress.setText(address);
     }
 }
